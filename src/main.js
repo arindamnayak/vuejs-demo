ab71b51bc09692137af34fce55f73261fddfe3ae
arindamnayak-spigit <arindam.nayak@spigit.com>
2018-02-08 18:45:31 +0530

    updated required dependency in vue

--- src/main.js
+++ src/main.js
@@ -2,15 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import BootstrapVue from "bootstrap-vue"
+//import BootstrapVue from "bootstrap-vue"
+import { Card, Navbar, FormInput, Dropdown } from 'bootstrap-vue/es/components';
 import VueResource from 'vue-resource'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 
-Vue.use(VueResource);
-Vue.use(BootstrapVue)
+Vue.use(VueResource)
+//Vue.use(BootstrapVue)
+Vue.use(Card)
+Vue.use(Navbar)
+Vue.use(FormInput)
+Vue.use(Dropdown)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
